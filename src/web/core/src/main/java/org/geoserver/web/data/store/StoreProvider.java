ba44b9cb0f449428f8774410f7dbfcc7df9d5691
n-lagomarsini <lago.88@libero.it>
2014-11-14 12:44:20 +0100

    Define GetFilter() as protected
    Conflicts:
    
    	src/web/demo/src/main/java/org/geoserver/web/demo/PreviewLayerProvider.java
    	src/web/gwc/src/main/java/org/geoserver/gwc/web/layer/UnconfiguredCachedLayersProvider.java

--- src/web/core/src/main/java/org/geoserver/web/data/store/StoreProvider.java
+++ src/web/core/src/main/java/org/geoserver/web/data/store/StoreProvider.java
@@ -229,25 +229,6 @@ public class StoreProvider extends GeoServerDataProvider<StoreInfo> {
 
         return items;
     }
-
-    private Filter getFilter() {
-        final String[] keywords = getKeywords();
-        Filter filter = acceptAll();
-        if (null != keywords) {
-            for (String keyword : keywords) {
-                Filter propContains = Predicates.fullTextSearch(keyword);
-                // chain the filters together
-                if (Filter.INCLUDE == filter) {
-                    filter = propContains;
-                } else {
-                    filter = or(filter, propContains);
-                }
-            }
-        }
-
-        
-        return filter;
-    }
     
     private Filter getWorkspaceFilter(Filter filter){
         // Filter by workspace if present
